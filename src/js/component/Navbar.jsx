8111b794252dd8d92f51a1db59816aec09b5ced7
Matteo Bertuzzi <146861948+matteobertuzzi@users.noreply.github.com>
2023-12-29 02:24:51 +0000

    Add Navbar component

---
+++ src/js/component/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark d-flex">
+            <a className="navbar-brand justify-content-start mx-4" href="#">Start Bootstrap</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse justify-content-end mx-4" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">About</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Services</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
