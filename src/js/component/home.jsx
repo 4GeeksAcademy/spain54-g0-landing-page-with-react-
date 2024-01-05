e6d307194295c0a63c084f91a9a57f8a0b23d6a9
Aitor <62630503+AitorMoyano@users.noreply.github.com>
2024-01-05 07:12:05 +0000

    feat:footer terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,61 @@
-import React from "react";
+import React from 'react';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
-const Home = () => {
+const Footer = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<footer>
+			<div class="row row-cols-1 row-cols-md-4 g-4">
+				<div class="col text-center">
+					<div class="card h-100">
+						<svg class="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg>
+						<div class="card-body">
+							<h5 class="card-title">Card title</h5>
+							<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sit amet sem placerat, lobortis lectus a, dignissim magna.</p>
+						</div>
+						<div class="card-footer">
+						<button type="button" class="btn btn-primary mx-auto p-2">Find Out More!</button>
+						</div>
+					</div>
+				</div>
+				<div class="col text-center">
+					<div class="card h-100">
+						<svg class="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg>
+						<div class="card-body">
+							<h5 class="card-title">Card title</h5>
+							<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sit amet sem placerat, lobortis lectus a, dignissim magna.</p>
+						</div>
+						<div class="card-footer">
+						<button type="button" class="btn btn-primary mx-auto p-2">Find Out More!</button>
+						</div>
+					</div>
+				</div>
+				<div class="col text-center">
+					<div class="card h-100">
+						<svg class="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg>
+						<div class="card-body">
+							<h5 class="card-title">Card title</h5>
+							<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sit amet sem placerat, lobortis lectus a, dignissim magna.</p>
+						</div>
+						<div class="card-footer">
+						<button type="button" class="btn btn-primary mx-auto p-2">Find Out More!</button>
+						</div>
+					</div>
+				</div>
+				<div class="col text-center">
+					<div class="card h-100">
+						<svg class="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg>
+						<div class="card-body">
+							<h5 class="card-title">Card title</h5>
+							<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sit amet sem placerat, lobortis lectus a, dignissim magna.</p>
+						</div>
+						<div class="card-footer">
+						<button type="button" class="btn btn-primary mx-auto p-2">Find Out More!</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
 	);
 };
 
-export default Home;
+export default Footer;
+<button type="button" class="btn btn-primary">Find Out More!</button>
