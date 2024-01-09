172f13e32cc1471da651317c269703b1d728ef4d
Fran Julian <138630369+Franjjr@users.noreply.github.com>
2024-01-09 14:08:03 +0000

    Feat: añado el ultimo componente, eran 4 no 3

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -12,7 +12,7 @@ export const Card = () => {
             bulk of the card's content.
           </p>
           <a href="#" className="btn btn-primary">
-            Go somewhere
+            Ir al sitio 1
           </a>
         </div>
       </div>
@@ -21,24 +21,35 @@ export const Card = () => {
         <div className="card-body">
           <h5 className="card-title">Card title 2</h5>
           <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+            lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           </p>
           <a href="#" className="btn btn-primary">
-            Go somewhere
+            Ir al sitio 2
           </a>
         </div>
       </div>
       <div className="card" style="width: 18rem;">
         <img src="..." className="card-img-top" alt="..." />
         <div className="card-body">
-          <h5 className="card-title">Card title 1</h5>
+          <h5 className="card-title">Card title 3</h5>
           <p className="card-text">
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
           </p>
           <a href="#" className="btn btn-primary">
-            Go somewhere
+            Ir al Sitio 3
+          </a>
+        </div>
+      </div>
+      <div className="card" style="width: 18rem;">
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title 4</h5>
+          <p className="card-text">
+            lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          </p>
+          <a href="#" className="btn btn-primary">
+            Ir al Sitio 4
           </a>
         </div>
       </div>
