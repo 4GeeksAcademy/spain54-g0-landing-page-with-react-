e566bac59d4ff30871613be432703e4a93f9730a
Matteo Bertuzzi <146861948+matteobertuzzi@users.noreply.github.com>
2024-01-09 18:41:26 +0100

    Update Jumbotron.jsx

--- src/js/component/Jumbotron.jsx
+++ src/js/component/Jumbotron.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Jumbotron = () => {
   return (
     <div className="p-5 mb-4 bg-light rounded-3">
-      <div clasName="container-fluid py-5">
+      <div className="container-fluid py-5">
         <h1 className="display-5 fw-bold">Custom jumbotron</h1>
         <p className="col-md-8 fs-4">
           Using a series of utilities, you can create this jumbotron, just like
