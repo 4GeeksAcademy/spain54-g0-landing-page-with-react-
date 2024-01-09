40d96dfee2b2408b7981430f75be8bff034dc914
Matteo Bertuzzi <146861948+matteobertuzzi@users.noreply.github.com>
2024-01-09 18:23:40 +0000

    update cards

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Card = () => {
   return (
-    <div className="d-flex justify-content-center">
+    <div className="container-fluid d-flex justify-content-center">
       <div className="card my-2 mx-2" style={{ width: "18rem" }}>
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Sunflower_from_Silesia2.jpg/1280px-Sunflower_from_Silesia2.jpg" className="card-img-top" alt="..." />
         <div className="card-body">
